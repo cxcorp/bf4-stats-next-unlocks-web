46d47f4022199f51f22e09af5c50d2332785d012
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2020-05-03 02:26:45 +0300

    Show approximate unlock name

--- data/selectedWeaponAccessory.js
+++ data/selectedWeaponAccessory.js
@@ -4,3 +4,7 @@ export const unlockProgress = (acc) => {
   const { actualValue, valueNeeded } = acc.weaponAddonUnlock.unlockedBy;
   return { actualValue, valueNeeded };
 };
+export const attachmentName = (acc) => {
+  const parts = (acc.weaponAddonUnlock.image || "").split("_");
+  return parts.slice(0, parts.length - 1).join(" ");
+};
