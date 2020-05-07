cxcorp/bf4-stats-next-unlocks-web

commit 10fe200292fb72716107044b949b09453548cf2e

Make unlocks table font smaller

components/UnlocksTable.js
@@ -1,5 +1,6 @@
 import { useState, useCallback, useMemo } from "react";
 import { Col, Table, Button, Form } from "react-bootstrap";
+import css from "styled-jsx/css";
 
 import { usePersistedState } from "~/util/hooks";
 import { maxBy, toLookup, setValuesTo } from "~/util";
@@ -233,6 +234,16 @@ const SortableTh = ({
   );
 };
 
+const unlocksTableStyle = css.resolve`
+  table {
+    font-size: 0.9rem;
+  }
+
+  .favorites {
+    border-bottom: 5px solid #fff;
+  }
+`;
+
 const UnlocksTable = ({
   unlocks,
   favoriteUnlocks,
@@ -258,7 +269,12 @@ const UnlocksTable = ({
 
   return (
     <>
-      <Table striped bordered responsive>
+      <Table
+        className={unlocksTableStyle.className}
+        striped
+        bordered
+        responsive
+      >
         <thead>
           <tr>
             <SortableTh {...getSortableProps(Sorters.KillsNeeded)}>
@@ -278,11 +294,7 @@ const UnlocksTable = ({
         <tbody className="favorites">{favoriteUnlocks.map(makeRow)}</tbody>
         <tbody>{unlocks.map(makeRow)}</tbody>
       </Table>
-      <style jsx>{`
-        .favorites {
-          border-bottom: 5px solid #fff;
-        }
-      `}</style>
+      {unlocksTableStyle.styles}
     </>
   );
 };

commit b73ade70c03912866abd680f78a0355e0fbf8a43

Fix favorites border style broken by 10fe2002

components/UnlocksTable.js
@@ -238,10 +238,6 @@ const unlocksTableStyle = css.resolve`
   table {
     font-size: 0.9rem;
   }
-
-  .favorites {
-    border-bottom: 5px solid #fff;
-  }
 `;
 
 const UnlocksTable = ({
@@ -294,6 +290,11 @@ const UnlocksTable = ({
         <tbody className="favorites">{favoriteUnlocks.map(makeRow)}</tbody>
         <tbody>{unlocks.map(makeRow)}</tbody>
       </Table>
+      <style jsx>{`
+        .favorites {
+          border-bottom: 5px solid #fff;
+        }
+      `}</style>
       {unlocksTableStyle.styles}
     </>
   );